70c07a13eecf30ee962e25b0c64d6e20016075db
LightDelight <second014785@gmail.com>
2025-11-03 11:14:42 +0900

    GAS 플러그인 및 모듈 추가, 블루프린트 병합

--- Source/Pair_A_Dies_Park/Pair_A_Dies_Park.Build.cs
+++ Source/Pair_A_Dies_Park/Pair_A_Dies_Park.Build.cs
@@ -8,6 +8,6 @@ public class Pair_A_Dies_Park : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 	}
 }
